72a364ec04f781be3fbd107b52df2241361bf74d
Elton Andrew <elton.andrew@airpay.co.in>
2020-05-30 18:07:44 +0530

    Burger component, Burger Ingredient Component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = props => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => [...Array(props.ingredients[igKey])]
+            .map((_, i) => <BurgerIngredient key={igKey + '_' + i} type={igKey} />))
+        .reduce((arr, el) => arr.concat(el), []);
+
+    if(transformedIngredients.length === 0){
+        transformedIngredients.push(<p key="Burger_notification">Please start adding ingredients!</p>);
+    }
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    )
+}
+
+export default burger
